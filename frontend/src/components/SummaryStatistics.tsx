1c08265200b20769b4e6cdc28559e5d0b353b3b1
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2025-03-15 11:39:08 -0700

    Convert to ts

--- frontend/src/components/SummaryStatistics.tsx
+++ frontend/src/components/SummaryStatistics.tsx
@@ -1,4 +1,12 @@
+import React from "react";
 import summary_statistics from "../data/summary_statistics.json";
+
+interface SummaryStatsData {
+    columns: string[];
+    data: (string | number)[][];
+}
+
+const summaryStatistics: SummaryStatsData = summary_statistics;
 
 function SummaryStatistics() {
     return (
@@ -7,20 +15,20 @@ function SummaryStatistics() {
             <table>
                 <thead>
                     <tr>
-                        {summary_statistics.columns.map((column, index) => (
+                        {summary_statistics.columns.map((column:string, index:number) => (
                             <th key={index}>{column}</th>
                         ))}
                     </tr>
                 </thead>
                 <tbody>
-                    {summary_statistics.data.map((row, index) => (
+                    {summary_statistics.data.map((row: (string|number)[], index:number) => (
                         <tr key={index}>
                             <td>{row[0]}</td>
                             <td>{row[1]}</td>
                             <td>{row[2]}</td>
-                            <td>{row[3].toFixed(5)}</td>
-                            <td>{row[4].toFixed(5)}</td>
-                            <td>{row[5].toFixed(1)}</td>
+                            <td>{typeof row[3] === 'number' ? row[3].toFixed(5) : row[3]}</td>
+                            <td>{typeof row[4] === 'number' ? row[4].toFixed(5) : row[4]}</td>
+                            <td>{typeof row[5] === 'number' ? row[5].toFixed(1) : row[5]}</td>
                         </tr>
                     ))}
                 </tbody>
