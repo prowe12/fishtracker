badd20470e8ebeb9284df77660577701dc55e9ab
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2025-03-15 12:03:40 -0700

    Add type declarations

--- frontend/src/components/Legend.tsx
+++ frontend/src/components/Legend.tsx
@@ -1,49 +1,55 @@
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import { useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
 import getColors from '../utils/getColors';
 
-const Legend = ({groups, species, compareValue}) => {
+interface LegendProps {
+  groups: string[];
+  species: string[];
+  compareValue: string;
+}
+
+const Legend: React.FC<LegendProps> = ({groups, species, compareValue}) => {
     const map = useMap();
   
     useEffect(() => {
       if (map) {
-        var legend = L.control({ position: 'topright' });
-        legend.onAdd = function (map) {
-        var div = L.DomUtil.create('div', 'info legend');
-        var labels = ['<strong>Categories</strong>'];
+        var legend = new L.Control({ position: 'topright' });
+        legend.onAdd = function () {
+          var div = L.DomUtil.create('div', 'info legend');
+          var labels = ['<strong>Categories</strong>'];
 
-        var categories = [];
-        if (compareValue === 'option1') {
-            // Show one color for each group
-            categories = groups;
-        } else if (compareValue === 'option2') {
-            // Show one color for each species
-            categories = species;
-        }
-        else if (compareValue === 'option3') {
-            // Show one color for each group + species
-            categories = [];
-            groups.forEach(group => {
-                species.forEach(species0 => {
-                    categories.push(group + ' ' + species0);
-                });
-            });
-        } else {
-            // Show one color regardless of groups and species
-            categories = ['All Fish'];
-        }
-          
-          for (var i = 0; i < categories.length; i++) {
-            labels.push(
-              '<i style="background:' + getColors(categories[i]) + '"></i> ' +
-              (categories[i] ? categories[i] : '+')
-            );
+          var categories: string[] = [];
+          if (compareValue === 'option1') {
+              // Show one color for each group
+              categories = groups;
+          } else if (compareValue === 'option2') {
+              // Show one color for each species
+              categories = species;
           }
-          div.innerHTML = labels.join('<br>');
-          return div;
+          else if (compareValue === 'option3') {
+              // Show one color for each group + species
+              categories = [];
+              groups.forEach(group => {
+                  species.forEach(species0 => {
+                      categories.push(group + ' ' + species0);
+                  });
+              });
+          } else {
+              // Show one color regardless of groups and species
+              categories = ['All Fish'];
+          }
+            
+            for (var i = 0; i < categories.length; i++) {
+              labels.push(
+                '<i style="background:' + getColors(categories[i]) + '"></i> ' +
+                (categories[i] ? categories[i] : '+')
+              );
+            }
+            div.innerHTML = labels.join('<br>');
+            return div;
         };
         legend.addTo(map);
   
